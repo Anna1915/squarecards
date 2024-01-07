dbf36645d7fa2b2d5ae9e91e38c1a7a30aaa8d0f
Anna1915 <155919741+Anna1915@users.noreply.github.com>
2024-01-07 23:11:05 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+})
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+})
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", function(){
+    window.open("https://www.youtube.com/watch?v=MKgDQjZwI2o");
+})
+
+document.getElementsByClassName("article")[0].addEventListener("click", function(){
+    window.open("https://iamcodefoxx.github.io/Weather-App/");
+})
